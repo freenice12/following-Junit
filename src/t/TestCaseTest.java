105d1cca122217e670996b5493f7489f4271f27b
freenice12 <freenice12@gmail.com>
2017-01-06 16:23:57 +0900

    Update and rename TestCaseTest to TestCaseTest.java

--- src/t/TestCaseTest.java
+++ src/t/TestCaseTest.java
@@ -8,45 +8,51 @@ public class TestCaseTest extends TestCase {
 
     public static void main(final String[] args) throws Exception {
         final TestSuite suite = new TestSuite();
-        suite.add(new TestCaseTest("testTemplateMethod"));
-        suite.add(new TestCaseTest("testResult"));
-        suite.add(new TestCaseTest("testFailedResultFormatting"));
-        suite.add(new TestCaseTest("testFailedResult"));
-        suite.add(new TestCaseTest("testSuite"));
-        suite.run();
+        suite.addTestSuite(TestCaseTest.class);
+        final TestResult result = new TestResult();
+        suite.run(result);
+        TestCase.assertEquals("6 run, 0 failed", result.summary());
     }
 
     public void testTemplateMethod() throws Exception {
         final WasRun test = new WasRun("testMethod");
         test.run();
-        assert ("setUp testMethod tearDown".equals(test.log));
+        assertEquals("setUp testMethod tearDown", test.log);
     }
 
     public void testResult() throws Exception {
         final WasRun test = new WasRun("testMethod");
         final TestResult result = test.run();
-        assert ("1 run, 0 failed".equals(result.summary()));
+        assertEquals("1 run, 0 failed", result.summary());
     }
 
     public void testFailedResult() throws Exception {
         final WasRun test = new WasRun("testBrokenMethod");
         final TestResult result = test.run();
-        assert ("1 run, 1 failed".equals(result.summary()));
+        assertEquals("1 run, 1 failed", result.summary());
     }
 
     public void testFailedResultFormatting() throws Exception {
         final TestResult result = new TestResult();
         result.testStarted();
         result.testFailed();
-        assert ("1 run, 1 failed".equals(result.summary()));
+        assertEquals("1 run, 1 failed", result.summary());
     }
 
     public void testSuite() throws Exception {
         final TestSuite suite = new TestSuite();
         suite.add(new WasRun("testMethod"));
         suite.add(new WasRun("testBrokenMethod"));
-        final TestResult result = suite.run();
-        assert ("2 run, 1 failed".equals(result.summary()));
+        final TestResult result = new TestResult();
+        suite.run(result);
+        assertEquals("2 run, 1 failed", result.summary());
     }
 
+    public void testAddTestSuite() throws Exception {
+        final TestSuite suite = new TestSuite();
+        suite.addTestSuite(WasRun.class);
+        final TestResult result = new TestResult();
+        suite.run(result);
+        assertEquals("1 run, 0 failed", result.summary());
+    }
 }
